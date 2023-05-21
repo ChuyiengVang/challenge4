85e45c93999c2fded86ca61b8065bae44954af88
Chue Vang <chuyieng@gmail.com>
2023-05-21 10:16:16 -0500

    timer working

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,26 +1,68 @@
 var questOne = {
-
+    questn: "Question number 1",
+    answer1: "answer",
+    answer2: "answer",
+    answer2: "answer",
+    answer4: "answer"
 }
 
 var questTwo = {
-
+    questn: "Question number 2",
+    answer1: "answer",
+    answer2: "answer",
+    answer2: "answer",
+    answer4: "answer"
 }
 
 var questThree = {
-
+    questn: "Question number 3",
+    answer1: "answer",
+    answer2: "answer",
+    answer2: "answer",
+    answer4: "answer"
 }
 
 var questFour = {
-
+    questn: "Question number 4",
+    answer1: "answer",
+    answer2: "answer",
+    answer2: "answer",
+    answer4: "answer"
 }
 
 var questFive = {
-    
+    questn: "Question number 5",
+    answer1: "answer",
+    answer2: "answer",
+    answer2: "answer",
+    answer4: "answer"
 }
 
 
-var highScore = document.querySelector("#highscore");
-var timer = document.querySelector("#timer");
-var counter = document.querySelector("#count");
-var startBtn = document.querySelector("#start");
+var highScore = document.getElementById("highscore");
+var timer = document.getElementById("timer");
+var counter = document.getElementById("count");
+var startBtn = document.getElementById("start");
+var questions = document.getElementById("questn");
+var button1 = document.getElementById("btn1");
+var button2 = document.getElementById("btn2");
+var button3 = document.getElementById("btn3");
+var button4 = document.getElementById("btn4");
 
+var timerCount = 100;
+
+function quizStart(){
+
+  var timerStart = setInterval(function() {
+    timerCount--;
+    counter.textContent = timerCount;
+
+    if (timerCount <= 0) {
+        clearInterval(timerStart);
+    }
+
+  }, 1000);
+
+}
+
+startBtn.addEventListener("click", quizStart)
